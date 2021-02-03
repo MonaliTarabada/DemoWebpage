1660f8f415cdd9014af3fda7dfcbbe0df62f824f
Monali Tarabada <tarabada.monali@gmail.com>
2021-02-02 23:10:20 -0500

    Updated version 2.0

--- js/script.js
+++ js/script.js
@@ -1,34 +1,15 @@
+var extralinks = document.getElementById("extra_added_links");
 function onClickLogin(){
   openNav();
-  if (document.contains(document.getElementById("sideNavDiv"))) {
-    document.getElementById("sideNavDiv").remove();
-  }
-    var sideDiv = document.createElement("div");
-    sideDiv.id = "sideNavDiv"
-    var sideDivLink1 = document.createElement("a");
-    sideDivLink1.id = "newLink1";
-    sideDivLink1.href =  '#';
-    sideDivLink1.innerHTML = "Loans";
-    var sideDivLink2 = document.createElement("a");
-    sideDivLink2.id = "newLink2";
-    sideDivLink2.href =  '#';
-    sideDivLink2.innerHTML = "Finance";
-    var sideDivLink3 = document.createElement("a");
-    sideDivLink3.id = "newLink3";
-    sideDivLink3.href =  '#';
-    sideDivLink3.innerHTML = "Debt";
-    sideDiv.append(sideDivLink1);
-    sideDiv.append(sideDivLink2);
-    sideDiv.append(sideDivLink3);
-    document.getElementById("itemList").prepend(sideDiv);
-    window.scrollTo(0,document.body.scrollHeight);
-
+  extralinks = document.getElementById("extra_added_links");
+  extralinks.style.display = "block";
+  window.scrollTo(0,document.body.scrollHeight);
 }
   
 function openNav() {
     document.getElementById("mySidenav").style.width = "fit-content";
-}
-  
+    extralinks.style.display = "none";
+} 
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     if (document.contains(document.getElementById("sideNavDiv"))) {
